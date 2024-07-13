b9589e078ede2baeee0f65d6e2cb6bb4c87dd28f
Parad1st <lolotrek228678@gmail.com>
2024-07-13 22:36:22 +0300

    Java
    Обновление Utils.java так как обновится и клиент

--- app/src/main/java/com/Parad1st/game/core/Utils.java
+++ app/src/main/java/com/Parad1st/game/core/Utils.java
@@ -1,11 +1,18 @@
-package com.blackrussia.game.core;
+package com.byparad1st.game.core;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
 
-import static com.blackrussia.game.core.Config.GAME_PATH;
+import static com.byparad1st.game.core.Config.GAME_PATH;
 
 public class Utils extends AppCompatActivity {
     
@@ -17,6 +24,47 @@ public class Utils extends AppCompatActivity {
     {
         File file = new File(GAME_PATH+path);
         return file.exists();
+    }
+    
+    public static int getTextLength(TextView textView) {
+        TextPaint textPaint = new TextPaint();
+        textPaint.setTextSize(textView.getTextSize());
+        textPaint.setTypeface(textView.getTypeface());
+        return (int) textPaint.measureText(textView.getText().toString());
+    }
+    
+    public static void makeAllViewsVisible(ViewGroup viewGroup) {
+        viewGroup.setVisibility(View.VISIBLE);
+        for (int i = 0; i < viewGroup.getChildCount(); i++) {
+            if (viewGroup.getChildAt(i) instanceof ViewGroup) {
+                makeAllViewsVisible((ViewGroup) viewGroup.getChildAt(i));
+            } else {
+                viewGroup.getChildAt(i).setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    public static void changeTextViewWidth(TextView textView) {
+        textView.post(new Runnable() {
+            @Override
+            public void run() {
+                TextPaint textPaint = new TextPaint();
+                textPaint.setTextSize(textView.getTextSize());
+                textPaint.setTypeface(textView.getTypeface());
+                textPaint.measureText(textView.getText().toString());
+                StaticLayout staticLayout = new StaticLayout(textView.getText(), textPaint, 10000, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+                int lineCount = staticLayout.getLineCount();
+                float lineWidth = staticLayout.getLineWidth(0);
+                for (int i = 0; i < lineCount; i++) {
+                    if (staticLayout.getLineWidth(i) >= lineWidth) {
+                        lineWidth = staticLayout.getLineWidth(i) + 5.0f;
+                    }
+                }
+                ViewGroup.LayoutParams layoutParams = textView.getLayoutParams();
+                layoutParams.width = (int) lineWidth;
+                textView.setLayoutParams(layoutParams);
+            }
+        });
     }
 
     static int installgametype = 0;
